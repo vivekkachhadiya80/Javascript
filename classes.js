921ec44d814fa3d87c1c70e64487a9c762e27492
vivekkachhadiya <ifmdac80@gmail.com>
2022-12-27 18:37:02 +0530

    first edit

---
+++ classes.js
@@ -0,0 +1,18 @@
+document.getElementById("demo1").innerHTML="hello world";
+
+
+function myfunction(){
+    document.getElementById("demo2").innerHTML=10+10;
+    document.getElementById("demo3").innerHTML="hello";
+}
+
+/* variables var , let ,const************/
+/*var a=10;
+var a=20;
+var c=a-b;*/
+
+let a=10;
+a=20;   
+
+const c=10;
+document.getElementById("demo4").innerHTML=c;
